db4e3b1b89e6f0d5858425cae63dbbf10370fd86
Vamshikrishna Enjapuri <vamsikrishnaenjapuri@gmail.com>
2022-10-11 22:46:25 +0530

    token swap ui

--- src/components/TokenCreation/MintNewFungibleToken.tsx
+++ src/components/TokenCreation/MintNewFungibleToken.tsx
@@ -10,7 +10,7 @@ import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { clusterApiUrl, Connection, SystemProgram } from "@solana/web3.js";
 import React, { useState } from "react";
-import { mintNewFungibleTokenArgs, REWARD_MINT_GLITCH, ALPHA_OWNER_ATA } from "./AlphaTokenConfig";
+import { mintNewFungibleTokenArgs, REWARD_MINT_GLITCH, ALPHA_OWNER_ATA, REWARD_MINT_ALPHA } from "./AlphaTokenConfig";
 import { MintNewFungibleTokenArgs } from "./TokenInterface";
 import SwappingIcon from "../../assets/swapping_icon.png";
 import { sendTransactions } from '../../config/connection';
@@ -23,7 +23,9 @@ import { alphaTokenSwapPda,alphaPotPda } from "../../GrandProgramUtils/Associate
 
 function MintNewFungibleToken() {
   const [glitchTokenVal, setGlitchTokenVal] = useState(0);
+  const [glitchTokenBal, setGlitchTokenBal] = useState(0);
   const [alphaTokenVal, setAlphaTokenVal] = useState(0);
+  const [alphaTokenBal, setAlphaTokenBal] = useState(0);
   const wallet = useWallet();
 
   const changeGlitchToken = async (val:any) => {
@@ -36,14 +38,35 @@ function MintNewFungibleToken() {
       REWARD_MINT_GLITCH, // mint
       wallet?.publicKey! // owner
     );
-  
-    let tokenAmount = await connection.getTokenAccountBalance(ata);
-    if (glitchTokenVal == 0) {
-      setGlitchTokenVal(parseInt(tokenAmount.value.amount) / 10 ** tokenAmount.value.decimals);
-      setAlphaTokenVal(parseInt(tokenAmount.value.amount) / 10 ** tokenAmount.value.decimals);
-    }
+
+    let alpha_ata = await getAssociatedTokenAddress(
+      REWARD_MINT_ALPHA, // mint
+      wallet?.publicKey! // owner
+    );
+    try {
+      let tokenAmount = await connection.getTokenAccountBalance(ata);
+      let tokenAmountAlpha = await connection.getTokenAccountBalance(alpha_ata);
+      if (glitchTokenVal == 0) {
+        // setGlitchTokenVal(parseInt(tokenAmount.value.amount) / 10 ** tokenAmount.value.decimals);
+        setGlitchTokenBal(parseInt(tokenAmount.value.amount) / 10 ** tokenAmount.value.decimals);
+        // setAlphaTokenVal(parseInt(tokenAmount.value.amount) / 10 ** tokenAmount.value.decimals);
+        setAlphaTokenBal(parseInt(tokenAmountAlpha.value.amount) / 10 ** tokenAmountAlpha.value.decimals);
+      }
+    } catch (error) {
+      
+    }  
   }
   getTokenAccountBalanceFn();
+
+  const percentageBtn =async (str:string) => {
+    if (str == '50') {
+      let k = parseInt((glitchTokenBal / 2).toFixed(0));
+      setGlitchTokenVal(k);
+    }
+    else if (str == '100') {
+      setGlitchTokenVal(glitchTokenBal);
+    }
+  }
 
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
@@ -229,6 +252,10 @@ function MintNewFungibleToken() {
       <div className="swapping-box">
         <label className="swapping-label">Trade</label>
         <div className="from-token-box">
+          <div className="from-token-header">
+            <label className="from-token-text">From</label>
+            <label className="from-token-bal">Bal : {glitchTokenBal}</label>
+          </div>
           <div className="token-image-parent">
             <img
               className="token-image"
@@ -240,23 +267,33 @@ function MintNewFungibleToken() {
             <h2 className="token-name">GLITCH</h2>
           </div>
           <div className="token-count-parent">
+            <button className="bal-half-btn" onClick={() => percentageBtn('50')}>50%</button>
+            <button className="bal-half-btn" onClick={() => percentageBtn('100')}>100%</button>
             <input
               type="number"
               max={1000000}
               className="token-count-input"
               value={glitchTokenVal}
+              pattern="^[0-9]*[.,]?[0-9]{0,9}$"
+              inputMode='numeric'
+              min="0"
+              step="1e-9"
               onChange={(event) =>
                 changeGlitchToken(parseInt(event.target.value))
               }
             />
           </div>
         </div>
-        <div className="pull-left full-width">
+        <div className="pull-left full-width text-center">
           <div className="swap-icon-parent">
             <img src={SwappingIcon} className="swap-icon" alt="" />
           </div>
         </div>
         <div className="to-token-box">
+          <div className="from-token-header">
+            <label className="from-token-text">To</label>
+            <label className="from-token-bal">Bal : {alphaTokenBal}</label>
+          </div>
           <div className="token-image-parent">
             <img
               className="token-image"
@@ -287,11 +324,11 @@ function MintNewFungibleToken() {
           >
             Swap
           </button>
-          <button className="swap-btn"
+          {/* <button className="swap-btn"
             onClick={() => testingBtn(mintNewFungibleTokenArgs)}
           >
             Testing
-          </button>
+          </button> */}
         </div>
       </div>
       {/* <div className="gen-farm-stats">
